66f13b6603b3e50926810c1ec4a4f0baf28df4dd
Brian LeRoux <b@brian.io>
2018-11-12 17:21:02 -0800

    try out timeout

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,4 +1,8 @@
 exports.handler = async function http(request) {
+  console.log('never return')
+  setTimeout(function() {
+    console.log('calling inner function')
+  }, 4*1000)
   return {
     status: 201,
     type: 'text/html; charset=utf8',
